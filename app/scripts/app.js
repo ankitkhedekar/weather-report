af6fc128c6e3e52c2f83a2130bfae4086772d2e1
Ankit Khedekar <ankitkhedekar@outlook.com>
2015-08-05 17:27:45 +0530

    scaffolded app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weatherReportApp
+ * @description
+ * # weatherReportApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weatherReportApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherReportApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherReportApp
+ */
+angular.module('weatherReportApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
