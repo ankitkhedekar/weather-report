2422a9e3228ee9e6143b11188b4a119dc91a801b
Ankit Khedekar <ankitkhedekar@outlook.com>
2015-08-07 18:55:49 +0530

    weather api connected

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,10 @@
  * Controller of the weatherReportApp
  */
 angular.module('weatherReportApp')
-  .controller('MainCtrl', function ($scope, localStorageService) {
+  .controller('MainCtrl', function ($scope, localStorageService, $http) {
 
   	var tempHistory = localStorageService.get('tempHistory') || [];
+    $scope.nowTemp = 0;
 
   	function addTemp() {
       console.log('add');
@@ -26,11 +27,36 @@ angular.module('weatherReportApp')
     function removeTemp() {
       console.log('remove');
       tempHistory.pop();//remove the oldest from history
-    }	
+    }
 
-    $scope.now = { temp: Math.floor((Math.random() * 11) + 20), timestamp: new Date().toLocaleString()}; 
+    function slowIncrement(val, target, delay){
+      //console.log("here", $scope.nowTemp);
+      $scope.nowTemp = val;
+      if(val < target) {
+          val++;
+          delay = delay + 2;
+          setTimeout(function() { 
+              $scope.$apply(function(){slowIncrement(val, target, delay)});
+            }
+          , delay);          
+      }
+    }
 
-	$scope.temps = JSON.parse(JSON.stringify(tempHistory));//deepcopy instead copy by reference
+    $http.get('http://api.worldweatheronline.com/free/v2/weather.ashx?q=Mumbai&format=json&key=7e54abfe1cd41da95e72db91b4eb7').
+    then(function(response) {
+      //console.log(response.data.data.current_condition[0].temp_C);
+      $scope.now = { temp: response.data.data.current_condition[0].temp_C, timestamp: new Date().toLocaleString()};
+      addTemp();
+      slowIncrement($scope.nowTemp, $scope.now.temp, 5);
+    }, function(response) {
+      console.log(response);
+    });
 
-	addTemp();
+    /*For testing without API*/
+    /*$scope.now = { temp: Math.floor((Math.random() * 11) + 20), timestamp: new Date().toLocaleString()};
+    slowIncrement($scope.nowTemp, $scope.now.temp, 5);*/
+    
+    //$scope.temps = JSON.parse(JSON.stringify(tempHistory));//deepcopy instead copy by reference
+    $scope.temps = angular.copy(tempHistory)
+
   });
